4b3fa3d3a02f321827a00330f0ac39112cd65a39
AlterionX <ben.xu.cs@gmail.com>
2025-05-04 23:24:39 -0700

    update sample rate

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ mod audio;
 mod schema;
 mod db;
 
+use std::num::NonZeroUsize;
+
 use tracing as trc;
 use serenity::async_trait;
-use songbird::SerenityInit;
+use songbird::{driver::SampleRate, SerenityInit};
 
 #[tokio::main]
 async fn main() {
@@ -15,7 +17,11 @@ async fn main() {
     let mut discord = azel::build_client(
         cfg,
         cmd::generate_command_descriptions(),
-        |b| b.register_songbird()
+        |b| b.register_songbird_from_config(songbird::Config::default()
+            .playout_buffer_length(NonZeroUsize::new(50).unwrap())
+            .playout_spike_length(10)
+            .decode_sample_rate(SampleRate::Hz16000)
+        )
     ).await.expect("client to be built");
 
 
